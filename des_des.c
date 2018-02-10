a424dc8d1a88ebad4aab87764c94a58e1aec5478
Andriy BODNAR <abodnar@e1r5p5.unit.ua>
2018-02-10 14:13:09 +0200

    1 block crypting - OK

--- des_des.c
+++ des_des.c
@@ -27,6 +27,13 @@
 #define R28OF56(x)			x & 0xFFFFFFF
 #define JOINBITS(x, y, sz)	(x << sz) | y
 
+typedef struct	s_desecb
+{
+	size_t		input_len;
+	size_t		encryted;
+
+};
+
 uint8_t initial_shuffle[64] =
 	{58, 50, 42, 34, 26, 18, 10, 2,
 	60, 52, 44, 36, 28, 20, 12, 4,
@@ -69,6 +76,99 @@ uint8_t pc2[48] =
 	30, 40, 51, 45, 33, 48,
 	44, 49, 39, 56, 34, 53,
 	46, 42, 50, 36, 29, 32};
+
+uint8_t six_to_four1[64] =
+	{14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
+	0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
+	4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
+	15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13};
+
+uint8_t six_to_four2[64] =
+	{15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
+	3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
+	0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15,
+	13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9};
+
+uint8_t six_to_four3[64] =
+	{10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
+	13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
+	13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7,
+	1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12};
+
+uint8_t six_to_four4[64] =
+	{7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
+	13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
+	10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
+	3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14};
+
+uint8_t six_to_four5[64] =
+	{2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
+	14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6,
+	4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14,
+	11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3};
+
+uint8_t six_to_four6[64] =
+	{12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
+	10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8,
+	9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6,
+	4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13};
+
+uint8_t six_to_four7[64] = 
+	{4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
+	13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6,
+	1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2,
+	6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12};
+
+uint8_t six_to_four8[64] =
+	{13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
+	1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
+	7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
+	2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11};		
+
+uint8_t *(six_to_four[]) =
+	{six_to_four1,	six_to_four2, six_to_four3, six_to_four4,
+	six_to_four5, six_to_four6, six_to_four7, six_to_four8};
+
+uint8_t p_permut[32] =
+	{16, 7, 20, 21,
+	29, 12, 28, 17,
+	1, 15, 23, 26,
+	5, 18, 31, 10,
+	2, 8, 24, 14,
+	32, 27, 3, 9,
+	19, 13, 30, 6,
+	22, 11, 4, 25};
+
+
+char			*ft_string_from_bits(uint64_t inf)
+{
+	char		*res;
+	int			i;
+
+	res = ft_strnew(8);
+	i = -1;
+	while (++i < 8)
+		res[i] = inf >> (56 - i * 8) & 255;
+	ft_printf("res = %s\n", res);
+	return(res);
+}
+
+uint64_t		ft_s_boxes(uint64_t inf)
+{
+	int			i;
+	uint64_t	res;
+	uint64_t	temp;
+
+	res = 0;
+	i = -1;
+	while (++i < 8)
+	{
+		res <<= 4;
+		temp = inf >> (42 - i * 6) & 63;
+		res = res | six_to_four[i][(CENTR4OF6(temp)) | ((FL2OF6(temp)) << 4)];
+	}
+	return (res);
+}
 
 uint64_t		ft_shuffle_key(uint64_t key, uint8_t i)
 {
@@ -121,35 +221,29 @@ uint64_t	ft_input_to_bits(char *str)
 
 char	*ft_encoding_des(char *input, uint64_t key)
 {
-	char		*res;
 	uint64_t	converted;
 	int			i;
-	int			j;
 	uint64_t	left;
 	uint64_t	left_new;
 	uint64_t	right;
-
-	ft_printf("===== first key check\nmy: %.64b\nst: 0001001100110100010101110111100110011011101111001101111111110001\n\n", key);
-
 
 	i = -1;
 	converted = ft_input_to_bits(input);
 	converted = ft_permut(converted, initial_shuffle, 64, 64);
 	key = ft_permut(key, pc1, 56, 64);
-	ft_printf("===== key check after permute\nmy: %.56b\nst: 11110000110011001010101011110101010101100110011110001111\n\n", key);
-
 	left = L32OF64(converted);
 	while (++i < 16)
 	{
 		left_new = R32OF64(converted);
 		right = ft_permut(R32OF64(converted), expand_right, 48, 32);
-		right = right ^ ft_permut(ft_shuffle_key(key, key_shift[i]), pc2, 48, 56);
-		
-
-
+		key = ft_shuffle_key(key, key_shift[i]);
+		right = right ^ ft_permut(key, pc2, 48, 56);
+		right = ft_s_boxes(right);
+		right = ft_permut(right, p_permut, 32, 32);
+		right = right ^ left_new;
 		converted = JOINBITS(left_new, right, 32);
 	}
-	return (ft_strdup("OK"));
+	return (ft_string_from_bits(converted));
 }
 
 char		*ft_des_ebc(char *input, uint64_t key)
